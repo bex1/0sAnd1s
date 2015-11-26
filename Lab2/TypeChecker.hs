b5d5ab800ed19e0e3165c874ffdfdc66d83d167c
bex <danbacks@student.chalmers.se>
2015-11-26 02:44:05 +0100

    Type checker kinda done. Lacking comments

--- Lab2/TypeChecker.hs
+++ Lab2/TypeChecker.hs
@@ -8,110 +8,182 @@ import Control.Monad
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-type Environment      = (Functions, [Context])
-type Functions      = Map Id ([Type], Type)
-type Context  = Map Id Type
+type Environment = (Functions, [Context])
+type Functions   = Map Id ([Type], Type)
+type Context     = Map Id Type
+
+typecheck :: Program -> Err ()
+typecheck (PDefs definitions) =
+  do
+    environment  <- buildInitialSymbolTable emptyEnvironment
+    environment' <- buildSymbolTable environment definitions
+    checkDefinitions environment' definitions
+    return ()
+
+buildInitialSymbolTable :: Environment -> Err Environment
+buildInitialSymbolTable environment =
+  do
+    environment <- updateFunction environment (Id "printInt")    ([Type_int], Type_void)
+    environment <- updateFunction environment (Id "printDouble") ([Type_double], Type_void)
+    environment <- updateFunction environment (Id "readInt")     ([], Type_int)
+    updateFunction                environment (Id "readDouble")  ([], Type_int)
+
+buildSymbolTable :: Environment -> [Def] -> Err Environment
+buildSymbolTable environment [] = return environment
+buildSymbolTable environment (DFun returnType functionId arguments _:restDefinitions) =
+    do
+      environment' <- updateFunction environment functionId (map getType arguments, returnType)
+      buildSymbolTable environment' restDefinitions
+  where
+    getType (ADecl declarationType _) = declarationType
+
+checkDefinitions :: Environment -> [Def] -> Err Environment
+checkDefinitions = foldM checkDefinition
+
+checkDefinition :: Environment -> Def -> Err Environment
+checkDefinition environment (DFun returnType _ arguments statements) =
+    do
+      environment' <- foldM addArgument (newBlock environment) arguments
+      checkStatements environment' returnType statements
+  where
+    addArgument environment (ADecl argumentType argumentId) = updateVariable environment argumentId argumentType
+
+checkStatements :: Environment -> Type -> [Stm] -> Err Environment
+checkStatements environment _ [] = return environment
+checkStatements environment returnType (statement:statements) =
+  do
+    environment' <- checkStatement environment returnType statement
+    checkStatements environment' returnType statements
+
+checkStatement :: Environment -> Type -> Stm -> Err Environment
+checkStatement environment functionReturnType statement =
+    case statement of
+      SExp expression -> inferExpression environment expression >> return environment
+      SDecls variableType variableIds -> foldM (addVariable variableType) environment variableIds
+      SInit variableType variableId expression -> checkExpression environment expression variableType
+                                                  >> updateVariable environment variableId variableType
+      SReturn returnExpression -> checkExpression environment returnExpression functionReturnType >> return environment
+      SWhile conditionExpression statement' -> checkExpression environment conditionExpression Type_bool
+                                               >> checkStatement environment functionReturnType statement'
+      SBlock statements -> checkStatements (newBlock environment) functionReturnType statements >> return environment
+      SIfElse conditionExpression trueStatement falseStatement ->
+        do
+          checkExpression environment conditionExpression Type_bool
+          checkStatement environment functionReturnType trueStatement
+          checkStatement environment functionReturnType falseStatement
+          return environment
+  where
+    addVariable variableType environment variableId = updateVariable environment variableId variableType
+
+checkExpression :: Environment -> Exp -> Type -> Err ()
+checkExpression environment expression expectedType =
+  do
+    actualType <- inferExpression environment expression
+    when (actualType /= expectedType) $
+      fail $ printTree expression ++ " has type " ++ printTree actualType ++ " expected " ++ printTree expectedType
+
+inferExpression :: Environment -> Exp -> Err Type
+inferExpression environment expression =
+  case expression of
+    ETrue     -> return Type_bool
+    EFalse    -> return Type_bool
+    EInt _    -> return Type_int
+    EDouble _ -> return Type_double
+    EId variableId -> lookupVariable environment variableId
+    EApp functionId argumentExpressions ->
+      do
+        (expectedArgumentTypes, returnType) <- lookupFunction environment functionId
+        actualArgumentTypes <- mapM (inferExpression environment) argumentExpressions
+        if expectedArgumentTypes == actualArgumentTypes then
+          return returnType
+        else
+          fail $ printTree functionId ++ " called with argument types " ++ show actualArgumentTypes
+                 ++ ", but the required argument types are " ++ show expectedArgumentTypes
+    EPostIncr expression  -> inferUnaryExpression [Type_int, Type_double] environment expression
+    EPostDecr expression  -> inferUnaryExpression [Type_int, Type_double] environment expression
+    EPreIncr expression   -> inferUnaryExpression [Type_int, Type_double] environment expression
+    EPreDecr expression   -> inferUnaryExpression [Type_int, Type_double] environment expression
+    ETimes expression1 expression2 -> sameExpressionTypes expression1 expression2
+    EDiv expression1 expression2   -> sameExpressionTypes expression1 expression2
+    EPlus expression1 expression2  -> inferBinaryExpression [Type_int, Type_double] environment expression1 expression2
+    EMinus expression1 expression2 -> inferBinaryExpression [Type_int, Type_double] environment expression1 expression2
+    ELt expression1 expression2    -> sameExpressionTypes expression1 expression2 >> return Type_bool
+    EGt expression1 expression2    -> sameExpressionTypes expression1 expression2 >> return Type_bool
+    ELtEq expression1 expression2  -> sameExpressionTypes expression1 expression2 >> return Type_bool
+    EGtEq expression1 expression2  -> sameExpressionTypes expression1 expression2 >> return Type_bool
+    EEq expression1 expression2    -> sameExpressionTypes expression1 expression2 >> return Type_bool
+    ENEq expression1 expression2   -> sameExpressionTypes expression1 expression2 >> return Type_bool
+    EAnd expression1 expression2   -> bothExpressionsBool expression1 expression2
+    EOr expression1 expression2    -> bothExpressionsBool expression1 expression2
+    EAss (EId variableId) expression1 -> sameExpressionTypes (EId variableId) expression1
+                                         >> inferExpression environment (EId variableId)
+  where
+    sameExpressionTypes expression1 expression2 =
+      do
+        type1 <- inferExpression environment expression1
+        type2 <- inferExpression environment expression2
+        if type1 == type2 then
+          return type1
+        else
+          fail $ printTree expression1 ++ " has type " ++ printTree type1 ++ " but " ++ printTree expression2
+                 ++ " has type " ++ printTree type2
+    bothExpressionsBool expression1 expression2 =
+      do
+        type1 <- inferExpression environment expression1
+        type2 <- inferExpression environment expression2
+        if type1 == Type_bool && type2 == Type_bool then
+          return Type_bool
+        else
+          fail $ printTree expression1 ++ " has type " ++ printTree type1 ++ " and " ++ printTree expression2
+                 ++ " has type " ++ printTree type2 ++ ", but both should be bool."
+
+inferBinaryExpression :: [Type] -> Environment -> Exp -> Exp -> Err Type
+inferBinaryExpression types environment expression1 expression2 =
+  do
+    expression1Type <- inferExpression environment expression1
+    if expression1Type `elem` types then
+      checkExpression environment expression2 expression1Type >> return expression1Type
+    else
+      fail $ "Wrong type of expression" ++ printTree expression1
+
+inferUnaryExpression :: [Type] -> Environment -> Exp -> Err Type
+inferUnaryExpression types environment expression =
+  do
+    expressionType <- inferExpression environment expression
+    if expressionType `elem` types then
+      return expressionType
+    else
+      fail $ "Wrong type of expression" ++ printTree expression
 
 lookupFunction :: Environment -> Id -> Err ([Type], Type)
-lookupFunction env fid =
-  case Map.lookup fid (fst env) of
-    Just funtyps  -> return funtyps
-    Nothing       -> fail "Function name not declared"
+lookupFunction (functions, _) functionId =
+  case Map.lookup functionId functions of
+    Just functionTypes  -> return functionTypes
+    Nothing             -> fail $ "Function " ++ show functionId ++ " not declared."
 
 updateFunction :: Environment -> Id -> ([Type], Type) -> Err Environment
-updateFunction (functions, ctxs) f funtyps =
-  if Map.member f functions
-    --then fail $ "Function name " ++  peelId f ++ " is aready used"
-    then fail "Function name is aready used"
-    else return (Map.insert f funtyps functions, ctxs)
+updateFunction (functions, contexts) functionId functionTypes =
+  if Map.member functionId functions then
+    fail $ "Function name " ++ show functionId ++ " is aready declared in the current scope."
+  else
+    return (Map.insert functionId functionTypes functions, contexts)
 
 lookupVariable :: Environment -> Id -> Err Type
-lookupVariable (functions, topctx:restctxs) v =
-  case Map.lookup v topctx of
-    Just typ  -> return typ
-    Nothing   -> fail "Variable name not declared"
+lookupVariable (_, []) variableId = fail $ "Variable " ++ show variableId ++ " not declared."
+lookupVariable (functions, topContext:restContexts) variableId =
+  case Map.lookup variableId topContext of
+    Just variableType  -> return variableType
+    Nothing            -> lookupVariable (functions, restContexts) variableId
 
 updateVariable :: Environment -> Id -> Type -> Err Environment
-updateVariable (functions, topctx:restctxs) v typ =
-  if Map.member v topctx
-    then fail "Variable already declared in current block"
-    else return (functions, Map.insert v typ topctx:restctxs)
+updateVariable (functions, topContext:restContexts) variableId variableType =
+  if Map.member variableId topContext then
+    fail $ "Variable " ++ show variableId ++ " already declared in current block."
+  else
+    return (functions, Map.insert variableId variableType topContext:restContexts)
 
 newBlock :: Environment -> Environment
-newBlock (functions, ctxs) = (functions, Map.empty : ctxs)
+newBlock (functions, contexts) = (functions, Map.empty:contexts)
 
 emptyEnvironment :: Environment
 emptyEnvironment = (Map.empty, [])
-
-typecheck :: Program -> Err ()
-typecheck (PDefs defs) = do
-  builtenv <- foldM -- foldM takes three arguments, namely the exact three lines below
-    (\env (id, typs) -> updateFunction env (Id id) typs)
-    emptyEnvironment
-    [("printInt", ([Type_int], Type_void)), ("printDouble", ([Type_double], Type_void)), ("readInt", ([], Type_int)), ("readDouble", ([], Type_double))]
-    -- skipping adding user def funcs.
-  let globalenv = builtenv
-  mapM
-    (\(DFun outtyp id args stms) ->
-      let
-        retType = case outtyp of
-          Type_void -> Nothing
-          _         -> Just outtyp
-      in do
-        fbdyenv <- foldM -- foldM takes three arguments, namely the exact three lines below
-          (\env (ADecl atyp aid) -> updateVariable env aid atyp)
-          (newBlock globalenv)
-          args
-        typecheckStms fbdyenv retType stms
-    )
-    defs
-  return ()
-
-typecheckStms :: Environment -> Maybe Type -> [Stm] -> Err ()
-typecheckStms env Nothing   []              = return ()
-typecheckStms env (Just _)  []              = fail "Return stamement expected"
-typecheckStms env (Just retType) [laststm]  =
-  case laststm of
-    SReturn exp -> typecheckExp env retType exp
-    _           -> fail "Return stamement expected"
-typecheckStms env retType (stm : stms)      = do
-  env' <- typecheckStm env stm
-  typecheckStms env' retType stms
-
-typecheckStm :: Environment -> Stm -> Err Environment
-typecheckStm _    (SReturn _) = fail "Return statement not expeced here"
-typecheckStm env  (SExp exp)  = do
-  _ <- typeinfer env exp
-  return env
-
-typecheckExp :: Environment -> Type -> Exp -> Err ()
-typecheckExp env typ exp = do
-  inftyp <- typeinfer env exp
-  if typ == inftyp
-    then return ()
-    else fail $ "Expected type " ++ printTree typ ++ ", but found type " ++ printTree inftyp
-
-typeinfer :: Environment -> Exp -> Err Type
-typeinfer env exp =
-  case exp of
-    EId id            -> lookupVariable env id
-    EInt _            -> return Type_int
-    EDouble _         -> return Type_double
-    ETrue             -> return Type_bool
-    EFalse            -> return Type_bool
-    EPlus exp1 exp2   -> do
-      typecheckExp env Type_int exp1
-      typecheckExp env Type_int exp2
-      return Type_int
-    EMinus exp1 exp2  -> do
-      typecheckExp env Type_int exp1
-      typecheckExp env Type_int exp2
-      return Type_int
-    ETimes exp1 exp2  -> do
-      typecheckExp env Type_int exp1
-      typecheckExp env Type_int exp2
-      return Type_int
-    EDiv exp1 exp2    -> do
-      typecheckExp env Type_int exp1
-      typecheckExp env Type_int exp2
-      return Type_int
